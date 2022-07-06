6ca0eeeb2117209c8dd1a237f0e545be27618774
johnnychivers.co.uk <64025427+johnny-chivers@users.noreply.github.com>
2022-07-06 14:53:37 +0100

    Add files via upload

---
+++ hive_statements.sql
@@ -0,0 +1,30 @@
+-- create database
+CREATE DATABASE IF NOT EXISTS emrdb;
+
+-- create table; 
+CREATE EXTERNAL TABLE emrdb.customer
+    (
+    `CustomerID` 	INT, 	
+	`NameStyle` 	STRING,	
+	`Title`	 	STRING,
+	`FirstName`	STRING,
+	`MiddleName`	STRING,
+	`LastName`	STRING,
+	`Suffix`	STRING,
+	`CompanyName`	STRING,
+	`SalesPerson`	STRING,
+	`EmailAddress`	STRING,
+	`Phone`		STRING,
+	`PasswordHash`	STRING,
+	`PasswordSalt`	STRING,
+	`rowguid`	STRING,
+	`ModifiedDate`  STRING
+    )
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+STORED AS TEXTFILE
+LOCATION 's3://johnny-chivers-emr/customers/'
+TBLPROPERTIES ('skip.header.line.count'='1')
+;
+
+
